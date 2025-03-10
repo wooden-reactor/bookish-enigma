74622eafb6f27dd370fa10372f799d3bad34a9cf
wooden-reactor <sharpveins@gmail.com>
2025-03-10 19:15:28 -0400

    update

--- examples/part5-example_three/script.js
+++ examples/part5-example_three/script.js
@@ -1,7 +1,7 @@
 
 function preload(){
     img = loadImage('images/1.png');
-    gif = loadImage('images/dove.gif');
+    gif = loadImage('images/calculator2.gif');
 }
 
 function setup() {
@@ -16,9 +16,10 @@ function draw() {
     background(0);
     
     //image(img,mouseX, mouseY, 100, 100)
-    window.onload = function() {
-        image(img, mouseX, mouseY, 100, 100);
-    }
+    // window.onload = function() {
+        // image(img, mouseX, mouseY, 100, 100);
+        image(gif, mouseX, mouseY, 100, 100);
+    // }
  
 }
 
